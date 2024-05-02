bd819e69fdf068b43094fd55853ae63f8b0baa97
Amir <sadeghi.ah79@gmail.com>
2024-05-02 21:43:21 +0330

    feat: implement minecraft server update functionality

--- mcserver.sh
+++ mcserver.sh
@@ -2,9 +2,26 @@
 
 mcserver_dir="/opt/minecraft"
 
+if [ -t 1 ]; then
+    # Define colors
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"
+    YELLOW="\033[0;33m"
+    BLUE="\033[0;34m"
+    CYAN="\033[0;36m"
+    NC="\033[0m" # No Color
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    CYAN=""
+    NC=""
+fi
+
 # Function to display error message and exit
 function display_error {
-    echo "Error: $1" >&2
+    echo -e "${RED}Error: $1${NC}" >&2
     exit 1
 }
 
@@ -31,6 +48,35 @@ function install_package {
     fi
 }
 
+# Function to check Java version and install/update if necessary
+function install_or_update_java {
+    local java_version=$(java -version 2>&1 | grep version | awk '{print $3}' | tr -d \")
+
+    if [ -z "$java_version" ]; then
+        echo "Java not found. Installing JDK 22..."
+        wget -O /tmp/jdk-22_linux-x64_bin.deb https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.deb
+        dpkg -i /tmp/jdk-22_linux-x64_bin.deb
+        rm /tmp/jdk-22_linux-x64_bin.deb
+    elif [[ "$java_version" != 22* ]]; then
+        echo "Java version $java_version found. Updating to JDK 22..."
+        wget -O /tmp/jdk-22_linux-x64_bin.deb https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.deb
+        dpkg -i /tmp/jdk-22_linux-x64_bin.deb
+        rm /tmp/jdk-22_linux-x64_bin.deb
+    else
+        echo "Java is already installed."
+    fi
+}
+
+function get_latest_version {
+    local manifest_url="https://launchermeta.mojang.com/mc/game/version_manifest.json"
+    local manifest_data=$(curl -sSL "$manifest_url") || display_error "Failed to fetch version manifest JSON."
+    echo "$manifest_data" | jq -r '.latest.release' || display_error "Failed to get latest release version."
+}
+
+function get_current_version {
+    echo $(unzip -p "$mcserver_dir/minecraft_server.jar" version.json | grep "name" | grep -oP '(?<=name": ")[^"]+')
+}
+
 # Function to get the Minecraft server download URL for a specific version or the latest version
 function get_minecraft_server_url {
     local minecraft_version=$1
@@ -38,8 +84,7 @@ function get_minecraft_server_url {
     local manifest_data=$(curl -sSL "$manifest_url") || display_error "Failed to fetch version manifest JSON."
 
     if [ "$minecraft_version" = "latest" ]; then
-        local latest_release_version=$(echo "$manifest_data" | jq -r '.latest.release') || display_error "Failed to get latest release version."
-        minecraft_version="$latest_release_version"
+        minecraft_version="$(get_latest_version)"
     fi
 
     local version_object=$(echo "$manifest_data" | jq --arg version "$minecraft_version" '.versions[] | select(.id == $version)') || display_error "Failed to find version object for Minecraft version $minecraft_version."
@@ -55,6 +100,22 @@ function get_minecraft_server_url {
     echo "$server_download_url"
 }
 
+# Function to check for updates and update the server JAR file
+function update_minecraft {
+    echo "Checking for updates..."
+    latest_version=$(get_latest_version)
+    current_version=$(get_current_version)
+    if [[ "$current_version" != "$latest_version" ]]; then
+        echo "New version available: $latest_version"
+        echo "Updating ..."
+        wget -O "$mcserver_dir/minecraft_server.jar" "$(get_minecraft_server_url "$latest_version")" || display_error "Failed to update server JAR file."
+        systemctl restart "minecraft@mcserver"
+        echo "Minecraft server file updated successfully to version $latest_version."
+    else
+        echo "Server JAR file is already up to date."
+    fi
+}
+
 function setup_server {
     if [ -f "$mcserver_dir/minecraft_server.jar" ]; then
         display_error "Minecraft server is already installed. Setup aborted."
@@ -67,7 +128,7 @@ function setup_server {
     # Install required packages
     install_package "curl"
     install_package "jq"
-    install_package "openjdk-17-jdk"
+    install_or_update_java
     install_package "screen"
     install_package "ufw"
 
@@ -94,9 +155,6 @@ function setup_server {
 
     # Accept EULA
     echo "eula=true" >eula.txt || display_error "Failed to accept EULA."
-
-    # Launch Minecraft server
-    #screen -S "mc-$server_name" -d -m java -Xmx${allocated_memory}M -Xms${allocated_memory}M -jar minecraft_server.jar nogui || display_error "Failed to start Minecraft server."
 
     # Create systemd service unit file
     cat <<EOF >/etc/systemd/system/minecraft@mcserver.service
@@ -183,8 +241,14 @@ function restart_server {
 }
 
 # Function to check status of Minecraft server
-function status {
-    local server_properties="/opt/minecraft/server.properties"
+function server_info {
+    local server_properties="$mcserver_dir/server.properties"
+
+    local minecraft_installed="${RED}● Not Installed${NC}"
+    if [ -f "$mcserver_dir/minecraft_server.jar" ]; then
+        minecraft_installed="${GREEN}● Installed${NC}"
+    fi
+
     if [ ! -f "$server_properties" ]; then
         local server_port=25565
     else
@@ -197,9 +261,24 @@ function status {
     local server_status=$(systemctl is-active "minecraft@mcserver")
     if [ "$server_status" = "active" ]; then
         local ip_address=$(hostname -I | cut -d ' ' -f1)
-        echo ".:: Active  | Address: ${ip_address}:${server_port}"
+        echo -e "${BLUE}.:: Installation Status:${NC} $minecraft_installed"
+        echo -e "${BLUE}.:: Server Status:${NC} ${GREEN}● Active${NC}"
+        echo -e "${BLUE}.:: Server Address:${NC} ${YELLOW}${ip_address}${NC}:${RED}${server_port}${NC}"
+        echo -e "${BLUE}.:: Version:${NC} ${GREEN}$(get_current_version)${NC}"
     else
-        echo ".:: Inactive"
+        echo -e "${BLUE}.:: Installation Status:${NC} $minecraft_installed"
+        echo -e "${BLUE}.:: Server Status:${NC} ${RED}● Inactive${NC}"
+    fi
+}
+
+function display_note {
+    local terminal_width=$(tput cols)
+    local note="${YELLOW}Note: You can update server properties from the location /opt/minecraft/server.properties. After making changes, remember to restart the Minecraft server for the updates to take effect.${NC}"
+
+    if [ "$terminal_width" -lt ${#colored_note} ]; then
+        echo -e "${note:0:$terminal_width-3}..."
+    else
+        echo -e "$note"
     fi
 }
 
@@ -235,32 +314,34 @@ function update_memory {
 
     systemctl daemon-reload
     systemctl restart "minecraft@mcserver"
-    
+
     echo "Allocated memory updated to $allocated_memory MB."
 }
 
-# Main menu
 function main_menu {
     clear
-    echo "  _____ _                     ___ _      _____                     "
-    echo " |     |_|___ ___ ___ ___ ___|  _| |_   |   __|___ ___ _ _ ___ ___ "
-    echo " | | | | |   | -_|  _|  _| .'|  _|  _|  |__   | -_|  _| | | -_| _|"
-    echo " |_|_|_|_|_|_|___|___|_| |__,|_| |_|    |_____|___|_|  \_/|___|_|  "
-    echo "                                                                  "
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    status
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "1. Install"
-    echo "2. Uninstall"
-    echo "3. Start"
-    echo "4. Stop"
-    echo "5. Restart"
-    echo "6. Update Allocated Memory"
-    echo "7. Toggle Daily Server Restart (Currently $(get_cronjob_status))"
-    echo "8. Open screen (logs)"
-    echo "0. Exit"
+    echo -e "$GREEN"
+    echo -e "  _____ _                     ___ _      _____                     "
+    echo -e " |     |_|___ ___ ___ ___ ___|  _| |_   |   __|___ ___ _ _ ___ ___ "
+    echo -e " | | | | |   | -_|  _|  _| .'|  _|  _|  |__   | -_|  _| | | -_| _|"
+    echo -e " |_|_|_|_|_|_|___|___|_| |__,|_| |_|    |_____|___|_|  \_/|___|_|  "
+    echo -e "                                                                  "
+    echo -e "$NC"
+    echo -e "$BLUE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~$NC"
+    server_info
+    echo -e "$BLUE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~$NC"
+    echo -e "${CYAN}1.$NC Install"
+    echo -e "${CYAN}2.$NC Uninstall"
+    echo -e "${CYAN}3.$NC Start"
+    echo -e "${CYAN}4.$NC Stop"
+    echo -e "${CYAN}5.$NC Restart"
+    echo -e "${CYAN}6.$NC Update Allocated Memory"
+    echo -e "${CYAN}7.$NC Toggle Daily Server Restart (Currently $(get_cronjob_status))"
+    echo -e "${CYAN}8.$NC Open screen (logs)"
+    echo -e "${CYAN}9.$NC Update Minecraft Server"
+    echo -e "${RED}0. Exit$NC"
     echo $'\n'
-    echo $'Note: You can update server properties from the location `/opt/minecraft/server.properties`.\nAfter making changes, remember to restart the Minecraft server for the updates to take effect.'
+    display_note
     echo $'\n'
     prompt_user "Enter your choice" choice
     case $choice in
@@ -272,6 +353,7 @@ function main_menu {
     6) update_memory ;;
     7) toggle_cronjob ;;
     8) screen -r mc-server ;;
+    9) update_minecraft ;;
     0) exit ;;
     *) display_error "Invalid choice. Please enter a number from 1 to 6." ;;
     esac
@@ -282,7 +364,6 @@ if [[ $EUID -ne 0 ]]; then
     display_error "This script must be run as root."
 fi
 
-# Main loop
 while true; do
     main_menu
     read -rp "Press Enter to continue..."
